027d3588a0917d9fac34672ecdb77feba00b5a6a
Mai Thi My Van <van.mai21@pn.com.vn>
2021-03-25 11:08:25 +0700

    update seeder v1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,6 @@ class DatabaseSeeder extends Seeder
         $this->call(NotifyTypesSeeder::class);
         $this->call(SchedulesSeeder::class);
         $this->call(NotificationsSeeder::class);
+        $this->call(BinLocationSeeder::class);
     }
 }
