df951a0b1bb6658c0dfc1dd0eb81ae3cc5ac57f9
thanhlong <thanhlong.developer@gmail.com>
2021-05-17 00:23:37 +0700

    add code

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -64,5 +64,15 @@ class UserSeeder extends Seeder
                     'image' => 'long.png',
                     'role' => 'user',
                 ]);
+            DB::table("users")->insert(
+                [
+                    'email' => 'thanhlong@gmail.com',
+                    'username' => 'thanhlong2',
+                    'password' => bcrypt("thanhlong2"),
+                    'name' => 'Thanh Long',
+                    'address' => '101b Le Huu Trac, Da Nang',
+                    'image' => 'long.png',
+                    'role' => 'admin',
+                ]);
     }
 }
